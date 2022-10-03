fb9f35302e6af08d04d398f95965dfad0f8b72bc
Maruf Khan <whoismaruf@outlook.com>
2022-10-03 12:07:19 +0600

    sent user details with tokens

--- credentials/api/serializers.py
+++ credentials/api/serializers.py
@@ -1,4 +1,3 @@
-from dataclasses import fields
 from rest_framework.serializers import ModelSerializer
 from django.contrib.auth import get_user_model
 
--- credentials/api/views.py
+++ credentials/api/views.py
@@ -1,12 +1,17 @@
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.http import JsonResponse
+from credentials.api.serializers import CustomUserDetailSerializer
+from django.contrib.auth import get_user_model
 
 
 def get_tokens_for_user(request):
     refresh = RefreshToken.for_user(request.user)
     print(f'token for {request.user}',refresh.access_token)
+    q__set = get_user_model().objects.filter(email=request.user.email).first()
+    user_detil = CustomUserDetailSerializer(q__set)
     return JsonResponse({
         'refresh_token': str(refresh),
         'access_token': str(refresh.access_token),
+        'user': user_detil.data
     })
     
