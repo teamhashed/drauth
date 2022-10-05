ce6e5bfa6808799b296203bbaa66a8e7c0dbe38c
Maruf Khan <whoismaruf@outlook.com>
2022-10-05 23:44:41 +0600

    another fixes #2

--- root/settings.py
+++ root/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from tkinter.tix import Tree
 from corsheaders.defaults import default_headers
 from datetime import timedelta
 import environ
